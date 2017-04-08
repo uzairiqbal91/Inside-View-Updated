7e5aa179c9c31f5e09e629bf9de5dcdfd2fc9faf
mirzaumersaleem <mirzaumersaleem.com>
2017-04-08 22:03:52 +0500

    Survey App initial commit

---
+++ app/src/main/java/androidadvance/com/androidsurveyexample/Camera.java
@@ -0,0 +1,40 @@
+package androidadvance.com.androidsurveyexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Camera extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+        Button photoButton = (Button) this.findViewById(R.id.button1);
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+}

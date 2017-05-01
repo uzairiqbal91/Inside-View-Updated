47ef669e3c7b2bd161ed65cc30d07880c3819c78
mirzaumersaleem <mirzaumersaleem.com>
2017-05-01 22:45:11 +0500

    Survey Question added

---
+++ surveylib/src/main/java/com/androidadvance/androidsurvey/widgets/phills_result.java
@@ -0,0 +1,17 @@
+package com.androidadvance.androidsurvey.widgets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.androidadvance.androidsurvey.R;
+
+public class phills_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phills_result);
+
+
+
+    }
+}
